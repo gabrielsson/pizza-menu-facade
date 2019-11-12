64779ba9e02f03daf1f97ccd6a792e16b4d601b2
Max Gabrielsson <max.gabrielsson@gmail.com>
2019-11-12 23:22:22 +0100

    Springify (#4)
    * Refactor springify
    
    * unused imports

--- src/main/java/com/gabrielsson/configuration/GraphQLDataFetchers.java
+++ src/main/java/com/gabrielsson/configuration/GraphQLDataFetchers.java
@@ -3,21 +3,18 @@ package com.gabrielsson.configuration;
 import com.gabrielsson.api.IngredientsProvider;
 import com.gabrielsson.api.MenuProvider;
 import graphql.schema.DataFetcher;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 @Component
+@AllArgsConstructor
 public class GraphQLDataFetchers {
 
     private final IngredientsProvider ingredientsProvider;
     private final MenuProvider menuProvider;
-
-    public GraphQLDataFetchers(IngredientsProvider ingredientsProvider, MenuProvider menuProvider) {
-        this.ingredientsProvider = ingredientsProvider;
-        this.menuProvider = menuProvider;
-    }
 
     public DataFetcher getIngredientsFetcher() {
         return dataFetchingEnvironment -> ingredientsProvider.provide();
